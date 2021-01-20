6015f2e543d9a271f63857fd1514d5d29e912c40
dedeherman <dedehermanvpay@gmail.com>
2021-01-20 14:50:40 +0700

    Updates

---
+++ precache-manifest.bfad8356e596e17a4cb8195be0fbcad3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "557b47887bc17f93a7dde108a050a27d",
+    "url": "/media-player/index.html"
+  },
+  {
+    "revision": "02cda5e0f4559961ca09",
+    "url": "/media-player/static/css/main.d13af4f5.chunk.css"
+  },
+  {
+    "revision": "c6ddbc8fb17ec9225445",
+    "url": "/media-player/static/js/2.525e05d5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/media-player/static/js/2.525e05d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02cda5e0f4559961ca09",
+    "url": "/media-player/static/js/main.3810be5c.chunk.js"
+  },
+  {
+    "revision": "695cd8cb465794116741",
+    "url": "/media-player/static/js/runtime-main.d8365ff9.js"
+  }
+]);
